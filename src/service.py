4324b7f742b9c765fdfbbd6fc38d17ad64b90ffd
makseq-msi3 <makseq@gmail.com>
2019-06-12 19:00:21 +0300

    Prerelease.

--- src/service.py
+++ src/service.py
@@ -3,8 +3,10 @@
 import sys
 import flask
 import json  # it MUST be included after flask!
+import pandas as pd
 import heartex
 
+from datetime import datetime as datetime
 from flask import request
 from utils import exception_treatment, answer, log_config, log
 
@@ -43,7 +45,12 @@ def index():
 
 
 def get_tweets_from_database(query, start_date):
-    return []
+    """ Insert here request to your DB
+    """
+    return [
+        {'text': 'cat runs', 'replies': ['bad', 'bad'], 'created_at': 'Thu May 23 18:03:00 +0000 2019'},
+        {'text': 'dog stops', 'replies': ['awesome', 'super'], 'created_at': 'Fri Jun 07 13:45:00 +0000 2019'}
+    ]
 
 
 def resampling_by_time(times, values, period):
@@ -65,37 +72,55 @@ def resampling_by_time(times, values, period):
     return x, y
 
 
-def heartex_build_plot(data, threshold_score, period):
+def heartex_build_plot(data, threshold_score=0.5, period='1D'):
     """ 1 Read data
         2 Recognize sentiment using Heartex
         3 Collect positives and negatives
         4 Resampling by time (to have one point per day)
-        5 Save output json for chart
+        5 Format output json for chart
+
+        :param data: tweets with replies
+        :param threshold_score: use only replies with score is greater threshold, from 0.0 to 1.0
+        :param period: resampling period (pandas style)
     """
     # pack tasks for Heartex prediction
     request_data = []
-    for row in data:
-        for reply in row['replies']:
+    for tweet in data:
+        for reply in tweet['replies']:
             request_data.append({'text': reply})
 
     # heartex predict
-    predictions = heartex.api.run_predict(token=token, project=sentiment_project_id, data=request_data)
+    predictions = heartex.api.run_predict(token=token, project=sentiment_project_id, data=request_data).json()
+    print(predictions)
 
     # unpack tasks back
-
+    count = 0
+    for tweet in data:
+        tweet['predictions'] = []
+        for _ in tweet['replies']:
+            tweet['predictions'].append(predictions[count])
+            count += 1
 
     # collect score values (positives & negatives)
-    for i, p in enumerate(predictions.json()):
-        data[i]['value'] = 0
-        if p['score'] > threshold_score:
-            for row in p['result']:
-                if 'Positive' in row['value']['choices']:
-                    data[i]['value'] = +1
-                if 'Negative' in row['value']['choices']:
-                    data[i]['value'] = -1
+    for tweet in data:
+        total = 0
+        tweet['value'] = 0
+
+        for prediction in tweet['predictions']:
+            if prediction['score'] > threshold_score:
+                total += 1
+
+                if 'Positive' in prediction['result'][0]['value']['choices']:
+                    tweet['value'] += 1
+
+                if 'Negative' in prediction['result'][0]['value']['choices']:
+                    tweet['value'] -= 1
+
+            # normalize
+            tweet['value'] /= float(total)
 
     # resampling
-    times = [d['time'] for d in data]
+    times = [datetime.strptime(d['created_at'], '%a %b %d %H:%M:%S +0000 %Y').timestamp() for d in data]
     values = [d['value'] for d in data]
     x, y = resampling_by_time(times, values, period)
 
@@ -106,23 +131,22 @@ def heartex_build_plot(data, threshold_score, period):
 @exception_treatment
 def api_build_sentiment():
     """ Main service function: build sentiment analysis plot by query keyword.
+
     1. Get tweets and replies from database
-    2. Send replies to Heartex predict
+    2. Send replies to Heartex predict to get sentiment scores
     3. Process predictions to plots
 
     :return: json with plot data
     """
     query = request.args['query']
     start_date = request.args['start_date']
+
     tweets = get_tweets_from_database(query, start_date)
-
-
-
+    output = heartex_build_plot(tweets)
 
     log.info('New query: %s' % str(request.args))
-    return answer(200, 'ok', {'test': 123})
+    return answer(200, 'ok', output)
 
 
 if __name__ == "__main__":
-    # to use audio recording we need https connection: ssl_context
     app.run(host='0.0.0.0', port=c['port'], debug=c['debug'])
